856628a138559dc124d791986d3908f4e10282ea
xywu <xwu70@jhu.edu>
2021-03-08 02:19:02 -0500

    update vaccine allocation data and corresponding variables in base dictionary

--- Scripts/vaccine_allocation.py
+++ Scripts/vaccine_allocation.py
@@ -2,23 +2,56 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan 28 23:26:23 2021
-Moved from Jupyter Notebook to inspect and implement code consistently
+Python script to read and process CDC's weekly updated vaccine allocation data
 
 @author: Xingyun Wu
 """
 
 import pandas as pd
+import numpy as np
 from datetime import datetime
+import re
 
+# set update date
+dt = '20210302'
 
-# read data
-file_dir = '/Users/xywu/Documents/HPC_datahub/vaccine/COVID-19_Vaccine_Distribution_Allocations_by_Jurisdiction_-_'
-pfizer = pd.read_csv(file_dir + 'Pfizer.csv')
-pfizer = pfizer.pivot(index = 'Jurisdiction', columns = 'Week of Allocations', \
-             values = ['1st Dose Allocations', '2nd Dose Allocations'])
-moderna = pd.read_csv(file_dir + 'Moderna.csv')
-moderna = moderna.pivot(index = 'Jurisdiction', columns = 'Week of Allocations', \
-             values = ['1st Dose Allocations', '2nd Dose Allocations'])
+# set data file directory
+file_dir = '~/Documents/HPC_datahub/vaccine/history_raw/COVID-19_Vaccine_Distribution_Allocations_by_Jurisdiction_-_'
+
+def read_data(filedir, brand, update_date):
+    df1 = pd.read_csv(file_dir + brand + '_' + update_date + '.csv')
+    df1.columns = [x.lower() for x in df1.columns]
+    print(df1.shape)
+    
+    # parse the stars as notations
+    notes = []
+    for i in range(df1.shape[0]):
+        temp = df1.loc[i, 'jurisdiction']
+        temp_stars = re.findall('[\*]+', temp)
+        temp_notes = np.zeros(shape = 4, dtype = int)
+        for j in range(len(temp_stars)):
+            temp_notes[len(temp_stars[j]) - 1] += 1
+            print(i, temp_stars[j], temp_notes)
+        notes.append(temp_notes)
+    
+    # create a data frame for notations
+    df2 = pd.DataFrame(data = np.array(notes), columns = ['no_pfizer_because_ultra_cold_requirement',\
+                                               'receive_dose1_dose2_simultaneously',\
+                                               'sovereign_nation_supplement', \
+                                               'federal_entities'])
+    
+    # combine two data frames
+    df1 = df1.join(df2, how = 'outer')
+    df1 = df1.drop(columns = 'federal_entities')
+    if brand == 'Moderna':
+        df1 = df1.drop(columns = 'no_pfizer_because_ultra_cold_requirement')
+    cols = df1.columns
+    
+    # reshape from long to wide
+    df1['jurisdiction'] = df1['jurisdiction'].str.replace('[^\w\s]', '')
+    df1 = df1.pivot(index = 'jurisdiction', columns = 'week of allocations')
+
+    return df1, cols
 
 
 # define an auxiliary function to extrace date from a column name
@@ -33,57 +66,47 @@ def edit_colname(colname, brand):
     '''
     temp_date = colname[1]
     temp_date = datetime.strptime(temp_date, '%m/%d/%Y').strftime('%Y%m%d')
-    # first dose or second dose
-    temp_dose = colname[0]
-    if temp_dose == '1st Dose Allocations':
-        temp_dose = 'dose1'
-    elif temp_dose == '2nd Dose Allocations':
-        temp_dose = 'dose2'
-    rv = temp_date + '_allocation_' + brand + '_' + temp_dose
+    
+    # standardize column names for doses
+    temp_col = colname[0]
+    if temp_col == '1st dose allocations':
+        temp_col = 'allocation_dose1'
+    elif temp_col == '2nd dose allocations':
+        temp_col = 'allocation_dose2'
+    
+    # set return value
+    if temp_col == 'no_pfizer_because_ultra_cold_requirement':
+        rv = temp_col + '_' + temp_date
+    else:
+        rv = brand + '_' + temp_col + '_' + temp_date
 
     # return the output
     return rv
 
+
+## process Pfizer data
+pfizer, pfizer_cols = read_data(file_dir, 'Pfizer', dt)
 # modify column names of dataframe
 pfizer.columns = [edit_colname(x, 'pfizer') for x in pfizer.columns]
 pfizer.insert(loc = 0, column = 'jurisdiction', value = pfizer.index)
 pfizer.index = range(pfizer.shape[0])
+# reorder the columns according to date
+pfizer = pfizer[sorted(pfizer.columns)]
+
+## process Moderna data
+moderna, moderna_cols = read_data(file_dir, 'Moderna', dt)
+# modify column names of dataframe
 moderna.columns = [edit_colname(x, 'moderna') for x in moderna.columns]
 moderna.insert(loc = 0, column = 'jurisdiction', value = moderna.index)
 moderna.index = range(moderna.shape[0])
-
-
-## reorder the columns
-# pfizer
-dts_pfizer = sorted(set([x.split('_')[0] for x in pfizer.columns[1:]]))
-col_pfizer = [x + '_allocation_pfizer_dose' + y for x in dts_pfizer for y in ['1', '2']]
-col_pfizer = [pfizer.columns[0]] + col_pfizer
-pfizer = pfizer[col_pfizer]
-# moderna
-dts_moderna = sorted(set([x.split('_')[0] for x in moderna.columns[1:]]))
-col_moderna = [x + '_allocation_moderna_dose' + y for x in dts_moderna for y in ['1', '2']]
-col_moderna = [moderna.columns[0]] + col_moderna
-moderna = moderna[col_moderna]
+# reorder the columns according to date
+moderna = moderna[sorted(moderna.columns)]
 
 
 ## merge two dataframes
 df = pfizer.merge(moderna, left_on='jurisdiction', right_on='jurisdiction')
 
 
-## remove the US territory and federal entities rows
-list(df.jurisdiction)
-to_rm = ['American Samoa', 'American Samoa**', 'Federal Entities', 'Guam', \
-         'Guam**', 'Mariana Islands', 'Mariana Islands**', 'Marshall Islands',\
-         'Marshall Islands*', 'Micronesia', 'Micronesia*', 'Palau', 'Palau*',\
-         'Puerto Rico', 'U.S. Virgin Islands']
-to_rm_id = [df.index[df['jurisdiction']==x].tolist()[0] for x in to_rm]
-df['jurisdiction'][to_rm_id]
-df = df.drop(to_rm_id, axis=0)
-# combine separated rows
-df['jurisdiction'] = df['jurisdiction'].str.replace('[^\w\s]', '')
-df = df.groupby('jurisdiction',as_index=False).agg('sum')
-
-
 ## output to csv file
-df.to_csv('/Users/xywu/Documents/HPC_Datahub/vaccine/vaccine_allocation.csv', index=False)
+df.to_csv('~/Documents/GitHub/COVID_Datahub/Pandemic/vaccine_allocation.csv', index=False)
 
