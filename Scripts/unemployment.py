d297ff46d9562641ffc92c4fe62f6135cb62fea7
xywu <xwu70@jhu.edu>
2021-08-16 11:23:27 -0400

    update unemployment data & dictionary

--- Scripts/unemployment.py
+++ Scripts/unemployment.py
@@ -11,15 +11,15 @@ import re
 
 
 ## read unemployment_v2 data because unemployment_v3 data has some mismatched county names
-df0 = pd.read_csv('~/Desktop/Unemployment_v2.csv')
+df0 = pd.read_csv('~/Documents/GitHub/COVID_DataHub/Unemployment/unemployment_v4.csv')
 df0.columns
-# get rid of multiple row index that was saved in v2 by mistake
-df0 = df0.iloc[:, 5:]
-df0.sort_values(by=['fips'], inplace = True)
+# drop the last month in df0 because the last month is always preliminary data
+df0.drop(columns = 'unemployment_202104', inplace = True)
+df0.columns
 
 
 ## read current data
-df1 = pd.read_excel('~/Documents/ra/HPC_datahub/unemployment/laucntycur14_20210620.xlsx',\
+df1 = pd.read_excel('~/Documents/ra/HPC_datahub/unemployment/laucntycur14_20210802.xlsx',\
                    skiprows = [0, 1, 2, 3, 5, 45072, 45073, 45074],\
                    names = ['laus_code', 'stfips', 'ctyfips', 'loc', 'period',\
                             'laborforce', 'employed', 'unemployed', 'unemployment'])
@@ -50,15 +50,14 @@ df2.sort_values(by='fips', inplace = True)
 # reset index as sequence number
 df2.reset_index(drop = True, inplace = True)
 df2.rename({'stname': 'stabbr'}, axis = 1, inplace = True)
-#df2 = ctyfips.merge(df2, how = 'right', on = ['stfips', 'stabbr', 'ctyfips', 'ctyname', 'fips'])
 
 
 ## merge
 # get rid of duplicate columns => if both occurs in df0 and df2, keep those in df2 (newer)
 col_diff = list(df0.columns.difference(df2.columns))
-col_diff = ['stfips', 'ctyfips', 'ctyname', 'fips'] + col_diff
-df0 = df0[col_diff]
-# merge df0 an df2
+col_d0 = ['stfips', 'ctyfips', 'ctyname', 'fips'] + col_diff
+df0 = df0[col_d0]
+# merge df0 and df2
 fnl = df0.merge(df2, how = 'left', on = ['stfips', 'ctyfips', 'fips'])
 fnl.columns
 # see different county names: ctyname_x, ctyname_y
@@ -68,12 +67,7 @@ for i in range(fnl.shape[0]):
         t.append(i)
 fnl.iloc[t, :][['ctyname_x', 'ctyname_y']]
 # keep the original county name
-fnl.rename({'ctyname_x': 'ctyname', 'neighbor1_2017': 'neighbor01_2017', \
-            'neighbor2_2017': 'neighbor02_2017', 'neighbor3_2017': 'neighbor03_2017',\
-            'neighbor4_2017': 'neighbor04_2017', 'neighbor5_2017': 'neighbor05_2017',\
-            'neighbor6_2017': 'neighbor06_2017', 'neighbor7_2017': 'neighbor07_2017',\
-            'neighbor8_2017': 'neighbor08_2017', 'neighbor9_2017': 'neighbor09_2017'},\
-           axis = 1, inplace = True)
+fnl.rename({'ctyname_x': 'ctyname'}, axis = 1, inplace = True)
 fnl.drop(columns = 'ctyname_y', inplace = True)
 # reorder columns
 fips_col = ['stfips', 'stabbr', 'stname', 'ctyfips', 'ctyname', 'fips']
@@ -84,12 +78,12 @@ fnl.sort_values(by='fips', inplace = True)
 
 
 ## output data
-fnl.to_csv('~/Documents/GitHub/COVID_DataHub/Unemployment/unemployment_v4.csv', \
+fnl.to_csv('~/Documents/GitHub/COVID_DataHub/Unemployment/unemployment_v5.csv', \
            index = False, na_rep = '')
 
 
 # get data dictionary
-base_dict = pd.read_csv('~/Documents/GitHub/COVID_DataHub/Unemployment/unemployment_base_dictionary.csv')
+base_dict = pd.read_csv('~/Documents/ra/HPC_datahub/unemployment/unemployment_base_dictionary.csv')
 
 d = {'variable_name': [] , 'start_column': [], 'end_column': [],\
      'start_month': [], 'end_month': []}
@@ -124,5 +118,5 @@ fnl_dict
 fnl_dict = base_dict.merge(fnl_dict, how = 'outer', on = 'variable_name')
 
 # save results
-fnl_dict.to_csv('~/Documents/GitHub/COVID_DataHub/Unemployment/unemployment_dictionary_v4.csv',\
+fnl_dict.to_csv('~/Documents/GitHub/COVID_DataHub/Unemployment/unemployment_dictionary_v5.csv',\
                 index = False)
